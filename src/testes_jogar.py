3eaf2d55458fc693dd36bc9e2a2837444c175076
headdragon <31053391+headdragon@users.noreply.github.com>
2017-09-26 21:12:35 -0300

    Update testes_jogar.py

--- src/testes_jogar.py
+++ src/testes_jogar.py
@@ -7,21 +7,21 @@ jogador = 'X'
 jogovelha.inicializar()
 jogovelha.jogar(jogador, lin, col)
 jogo = jogovelha.tabuleiro()
-if len(jogo) != 3:
-erro = True
-else:
-for linha in range(0,3):
-if len(jogo[linha]) != 3:
-erro = True
-else:
-for coluna in range(0,3):
-if linha == lin and coluna == col:
-if jogo[linha][coluna] != jogador:
-erro = True
-elif jogo[linha][coluna] != '.':
-erro = True
+  if len(jogo) != 3:
+    erro = True
+  else:
+    for linha in range(0,3):
+      if len(jogo[linha]) != 3:
+        erro = True
+      else:
+        for coluna in range(0,3):
+          if linha == lin and coluna == col:
+            if jogo[linha][coluna] != jogador:
+              erro = True
+          elif jogo[linha][coluna] != '.':
+           erro = True
 if erro:
-print('Erro!')
-sys.exit(1)
+  print('Erro!')
+  sys.exit(1)
 else:
-sys.exit(0)
+  sys.exit(0)
